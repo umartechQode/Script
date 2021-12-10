3f078a473b59e27460df17fc7c06c4bba7ccc67d
AhxanFarooq <mahsangee265@outlook.com>
2021-12-10 15:24:49 +0500

    wip

--- script.sql
+++ script.sql
@@ -1,16 +1,12 @@
 ﻿ALTER TABLE [PrintSettings] ADD [InvoicePrint] nvarchar(max) NULL;
 
-GO
-
 UPDATE [Companies] SET [CreatedDate] = '2021-12-10T07:38:45.4963770Z'
 WHERE [Id] = '5f8d5614-2c7e-4ec0-868c-d254e6516b4d';
 SELECT @@ROWCOUNT;
 
 
-GO
-
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
 VALUES (N'20211210073847_invoice-print-setting-english-arabic', N'2.2.0-rtm-35687');
 
-GO
+
 
